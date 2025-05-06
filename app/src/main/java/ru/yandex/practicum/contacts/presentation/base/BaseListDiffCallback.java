bbd3ed7bcf0097b3b7a1451bd2d4962a29f3a580
neutrinna <melikhovainna@gmail.com>
2025-05-06 18:18:08 +0300

    generics create

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DiffUtil;
+
+public class BaseListDiffCallback<BaseListDiffCallback extends ListDiffInterface<BaseListDiffCallback>> extends DiffUtil.ItemCallback<BaseListDiffCallback> {
+    @Override
+    public boolean areItemsTheSame(@NonNull BaseListDiffCallback oldItem, @NonNull BaseListDiffCallback newItem) {
+        return oldItem.theSameAs(newItem);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull BaseListDiffCallback oldItem, @NonNull BaseListDiffCallback newItem) {
+        return oldItem.equals(newItem);
+    }
+
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull BaseListDiffCallback oldItem, @NonNull BaseListDiffCallback newItem) {
+        return newItem;
+    }
+}
